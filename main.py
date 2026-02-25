48ff4a68c6cbd66e236b9381ed12748347627316
Gaku Takahashi <gakut2002@gmail.com>
2026-02-25 10:38:53 +0900

    enabled to customize message form of all exceptions

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, HTTPException, Request
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+'''
+フィボナッチ行列のn項の値を取得する
+第1項を求める場合、その値は入力値n=1とnと合致するので、その値をそのまま返す。
+第n項(i>=2)については、第2項, 第3項,...第n項と順に求めて
+いけば求まるので、n-1回その計算を繰り返したら求まる(bがその値)
+'''
 def fib(n: int):
     if n <= 1:
         return n
@@ -11,16 +18,51 @@ def fib(n: int):
         a, b = b, a + b
     return b
 
+'''
+HTTPExceptionをそのまま出すと、レスポンスのjsonのキーが
+detail(エラーの詳細)だけになってしまうのでエラーメッセージをカスタマイズする
+'''
 @app.exception_handler(HTTPException)
-async def custom_http_exception_handler(request: Request, exc: HTTPException):
+async def value_exception_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=exc.status_code,
         content={
-            "status": "error",
+            "status": str(exc.status_code),
             "message": exc.detail
         },
     )
 
+'''
+fibエンドポイント以外にアクセスした場合にもカスタマイズしてレスポンスを返す
+'''
+@app.exception_handler(StarletteHTTPException)
+async def not_found_exception_handler(request: Request, exc: StarletteHTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "status": str(exc.status_code),
+            "message": exc.detail
+        },
+    )
+
+'''他のExceptionについてもカスタマイズしたメッセージを返す'''
+@app.exception_handler(Exception)
+async def general_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={
+            "status": "error",
+            "message": str(exc)
+        },
+    )
+
+'''
+fibエンドポイントにアクセスされた時に実行される
+入力した値が正の整数でないならその時点で「不正な値」を意味する
+エラーを出し、その内容をカスタマイズしてレスポンスとしてリクエスト側に返す
+他のエラーについても同じ
+エラーもなく正しい値を取得できたら、resultにその値を充てたstatus=200のjsonレスポンスを返す
+'''
 @app.get("/fib")
 def fibonacci(n: int):
     try:
